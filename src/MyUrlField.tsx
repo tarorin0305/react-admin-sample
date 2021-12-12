c43358b9738f2c1bea52dca169f0fe123cb155b2
tarorin0305 <dayutakatarorin@gmail.com>
2021-12-12 16:42:01 +0900

    add Style

--- src/MyUrlField.tsx
+++ src/MyUrlField.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
 import { useRecordContext} from 'react-admin';
+import { makeStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
 
-// {source: "value"}
+const useStyles = makeStyles({
+  link: {
+    textDecoration: 'none',
+  },
+  icon: {
+    width: '0.5em',
+    height: '0.5em',
+    paddingLeft: 2,
+  },
+})
 
 const MyUrlField = ({ source }: { source: any }) => {
   const record = useRecordContext();
+  const classes = useStyles();
   return record ? (
-    <a href={record[source]}>
+    <a href={record[source]} className={classes.link}>
       {record[source]}
+      <LaunchIcon className={classes.icon}/>
     </a>
   ) : null;
 }
