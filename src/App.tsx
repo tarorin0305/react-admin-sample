f1e36d9a0938b376a8075390a61bb693076e74d4
tarorin0305 <dayutakatarorin@gmail.com>
2021-12-13 21:40:09 +0900

    Add Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from './posts';
+import Dashboard from './Dashboard'
+import PostIcon from '@material-ui/icons/Book'
+import UserIcon from '@material-ui/icons/Group'
 import jsonServerProvider from 'ra-data-json-server';
 import './App.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    <Resource name="users" list={UserList} icon={UserIcon}/>
   </Admin>
 );
 
---
+++ src/Dashboard.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader} from '@material-ui/core';
+
+export default () => (
+  <Card>
+    <CardHeader title="Welcome to the Admin"/>
+    <CardContent>Admin Mode</CardContent>
+  </Card>
+)
