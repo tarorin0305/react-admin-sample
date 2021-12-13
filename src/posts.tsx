e1ddfecc9694c22fa09fd9b1ea2faec5bb390792
tarorin0305 <dayutakatarorin@gmail.com>
2021-12-13 21:32:26 +0900

    Add Search Filters

--- src/posts.tsx
+++ src/posts.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
 import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, Create, SimpleForm, ReferenceInput, SelectInput, TextInput } from 'react-admin';
 
+const PostTitle = ({ record }: { record: any}) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+}
+
+const postFilters = [
+  <TextInput source="q" label="Search" alwaysOn />,
+  <ReferenceInput source="userId" label="User" reference="users" allowEmpty>
+    <SelectInput optionText="name"/>
+  </ReferenceInput>
+];
+
 export const PostList = (props: any) => (
-  <List {...props}>
+  <List filters={postFilters} {...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
@@ -15,7 +26,7 @@ export const PostList = (props: any) => (
 )
 
 export const PostEdit = (props: any) => (
-  <Edit {...props}>
+  <Edit title={<PostTitle record={undefined} />} {...props}>
     <SimpleForm>
       <TextInput disabled source="id"/>
       <ReferenceInput source="userId" reference="users">
